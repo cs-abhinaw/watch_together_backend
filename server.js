6476b28611392f1698c6b1e56bd10b3eccc8dfc1
cs-abhinaw <abhinaw.kumar@91coderstreet.com>
2025-04-23 23:29:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://spiffy-fairy-8b205f.netlify.app",
     methods: ["GET", "POST"],
   },
 });
